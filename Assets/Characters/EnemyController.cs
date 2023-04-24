33981345cb3ade6ef3ddcfcccedff8bbc3699978
xuzhihuiCSY <xuzhihuieateat@gmail.com>
2023-04-24 14:54:05 -0700

    enemy updata
    now enemy stops moving and able to check if player is in range and attack player

--- Assets/Characters/EnemyController.cs
+++ Assets/Characters/EnemyController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+<<<<<<< Updated upstream:Assets/Characters/EnemyController.cs
     public GameObject character; // The player character
     public float moveSpeed = 1.0f; // The speed at which the enemy moves
+=======
+>>>>>>> Stashed changes:Assets/EnemyController.cs
     public float attackRange = 2.0f; // The distance at which the enemy attacks
     public int health = 1; // The health of the enemy
     public int attackDamage = 3; // The amount of damage the enemy deals
@@ -14,10 +17,18 @@ public class EnemyController : MonoBehaviour
 
     void Update()
     {
-        if (!hasMovedThisTurn)
+        if (!hasMovedThisTurn && !PlayerController.isPlayerTurn)
         {
+            Debug.Log("Now it is the enemy's turn.");
+
             // Check if the player character is within attack range
+<<<<<<< Updated upstream:Assets/Characters/EnemyController.cs
             float distanceToCharacter = Vector3.Distance(transform.position, character.transform.position);
+=======
+            float distanceToCharacter = Vector3.Distance(transform.position, playerCharacter.transform.position);
+            Debug.Log("Distance between enemy and player: " + distanceToCharacter); // Log the distance
+
+>>>>>>> Stashed changes:Assets/EnemyController.cs
             if (distanceToCharacter <= attackRange)
             {
                 // Attack the player character
@@ -27,25 +38,19 @@ public class EnemyController : MonoBehaviour
                 // End the enemy's turn after attacking
                 EndTurn();
             }
+<<<<<<< Updated upstream:Assets/Characters/EnemyController.cs
             else if (distanceToCharacter <= 5)
             {
                 // Move towards the player character if they are within 5 blocks
                 transform.position += (character.transform.position - transform.position).normalized * moveSpeed * Time.deltaTime;
                 Debug.Log("Enemy moving towards player character!");
             }
+=======
+>>>>>>> Stashed changes:Assets/EnemyController.cs
             else
             {
-                // Move right 5 blocks in the first turn, and left 5 blocks in the next turn
-                if (hasMovedThisTurn)
-                {
-                    transform.position += Vector3.left * 5;
-                    Debug.Log("Enemy moving left!");
-                }
-                else
-                {
-                    transform.position += Vector3.right * 5;
-                    Debug.Log("Enemy moving right!");
-                }
+                Debug.Log("Enemy didnt see player character");
+                EndTurn();
             }
 
             // Mark this turn as having been moved
@@ -53,14 +58,36 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+<<<<<<< Updated upstream:Assets/Characters/EnemyController.cs
     public void EndTurn()
+=======
+
+
+
+    private void Start()
+    {
+        playerScript = playerCharacter.GetComponent<PlayerController>();
+
+        if (playerScript == null)
+        {
+            Debug.LogError("No character found.");
+        }
+    }
+
+
+   public void EndTurn()
+>>>>>>> Stashed changes:Assets/EnemyController.cs
     {
         // Reset the hasMovedThisTurn flag at the end of each turn
         hasMovedThisTurn = false;
 
+        // Switch back to the player's turn
+        PlayerController.isPlayerTurn = true;
+
         // Output a message indicating the end of the enemy's turn
         Debug.Log("Enemy turn ended.");
     }
+
 
     public void DeleteEnemy()
     {
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,6 +7,16 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Transform movePoint;
+<<<<<<< Updated upstream
+=======
+    // must be "land" or "air" or "water
+    public string movementType;
+    public int attack;
+    public int health;
+    public static bool isPlayerTurn = true;
+    public Tilemap tilemap;
+    private GameObject[] enemies;
+>>>>>>> Stashed changes
 
     // move the movePoint in a given direction by 1 tile
     public void changeMovePoint(string direction)
@@ -55,6 +65,80 @@ public class PlayerController : MonoBehaviour
         else if (name == "AttackBlock")
         {
 
+<<<<<<< Updated upstream
+=======
+    // moves player again after previous move is done
+    public IEnumerator ActivatePlayer(List<string> actionStack)
+    {
+        foreach (string action in actionStack)
+        {
+            DoAction(action);
+
+            while (transform.position != movePoint.position)
+            {
+                MoveToPoint();
+                StartCoroutine(MoveToPoint());
+                yield return new WaitUntil(() => transform.position == movePoint.position); // wait for the next frame
+            }
+        }
+        PlayerController.isPlayerTurn = false; // End player's turn
+    }
+
+
+    // all enemies 1-grid square within this character takes damage
+    private void AttackAll()
+    {
+        List<GameObject> nearbyEnemies = GetCloseEnemies();
+        
+        foreach (var enemy in nearbyEnemies)
+        {
+            enemy.GetComponent<EnemyController>().TakeDamage(attack);
+        }
+    }
+
+    // return a list of enemies 1-grid square away
+    private List<GameObject> GetCloseEnemies()
+    {
+        List<GameObject> closeEnemies = new List<GameObject>();
+
+        foreach (var enemy in enemies)
+        {
+            if (Vector3.Distance(transform.position, enemy.transform.position) <= 1.2)
+            {
+                closeEnemies.Add(enemy);
+            }
+        }
+
+        return closeEnemies;
+    }
+
+    // called by other entities to make this one take damage
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    // check if tile to move to is passable, false if not
+    private bool CheckMoveTile()
+    {
+        Vector3Int movePointTile = tilemap.WorldToCell(movePoint.position);
+        TileBase tile = tilemap.GetTile(movePointTile);
+
+        if (tile is CustomTile customTile)
+        {
+            if (customTile.tileType != movementType)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return false;
+>>>>>>> Stashed changes
         }
     }
 
