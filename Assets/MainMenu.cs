1d8a6d59c15a9edda714afd36f3e410971f8d19c
dragoontrick <47570619+dragoontrick@users.noreply.github.com>
2023-05-05 21:55:06 -0700

    temppushherefirst

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -12,6 +12,12 @@ public class MainMenu : MonoBehaviour
 
     }
 
+    public void PrevLvl()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+    }
+
     public void QuitGame()
     {
         Debug.Log("Quit!");
